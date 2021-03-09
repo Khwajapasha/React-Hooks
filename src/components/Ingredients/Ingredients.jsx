d8e1a7171846b9f02f978c76d5be17f7eb0a2bdb
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-09 12:54:14 +0530

    removing ingredient onClick of list

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -5,11 +5,18 @@ import Search from "./Search";
 import IngredientList from "./IngredientList";
 const Ingredients = () => {
   const [userIngredient, setUserIngredient] = useState([]);
+  // Adding Ingredient Into List
   const addIngredientsHandler = (itemCurrent) => {
     setUserIngredient((prevIngredient) => [
       ...prevIngredient,
       { id: Math.random().toString(), ...itemCurrent },
     ]);
+  };
+  // Deleting Ingredient From List
+  const deleteIngredientHandler = (ingredientId) => {
+    setUserIngredient((prevIngredient) =>
+      prevIngredient.filter((ingredient) => ingredient.id !== ingredientId)
+    );
   };
   return (
     <div className="App">
@@ -17,7 +24,10 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList itemCurrent={userIngredient} onRemoveItem={() => {}} />
+        <IngredientList
+          itemCurrent={userIngredient}
+          onRemoveItem={deleteIngredientHandler}
+        />
       </section>
     </div>
   );
