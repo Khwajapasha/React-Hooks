6c764b2e01ffe14b3a4477cc6f4eab02e138842d
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-09 18:57:55 +0530

    update props name

--- src/components/Ingredients/IngredientList.jsx
+++ src/components/Ingredients/IngredientList.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./IngredientList.css";
 
-const IngredientList = (kuchBhi) => {
+const IngredientList = (props) => {
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
-        {kuchBhi.value.map((ig) => (
-          <li key={ig.id} onClick={kuchBhi.onRemoveItem.bind(this, ig.id)}>
+        {props.Ingredients.map((ig) => (
+          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
             <span>{ig.title}</span>
             <span>{ig.amount}x</span>
           </li>
