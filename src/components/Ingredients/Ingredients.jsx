7d2bf4aa7d8e809368e75fcea6f8599fbe44a550
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-10 15:29:37 +0530

    testing for loop

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -7,23 +7,22 @@ const Ingredients = () => {
   const [userIngredient, setUserIngredient] = useState([]);
 
   // Fetching Data from database
-  useEffect(() => {
-    fetch(
-      "https://react-hooks-49032-default-rtdb.firebaseio.com/IngredientsFromInput.json"
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredient = [];
-        for (const key in responseData) {
-          loadedIngredient.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        setUserIngredient(loadedIngredient);
-      });
-  }, []);
+  useEffect(() => {}, []);
+  fetch(
+    "https://react-hooks-49032-default-rtdb.firebaseio.com/IngredientsFromInput.json"
+  )
+    .then((response) => response.json())
+    .then((responseData) => {
+      const loadedIngredient = [];
+      for (const key in responseData) {
+        loadedIngredient.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      // setUserIngredient(loadedIngredient);
+    });
   // Adding Ingredient Into List
   // const ingredientGUID = uuidv4();
   const addIngredientsHandler = (Ingredients) => {
