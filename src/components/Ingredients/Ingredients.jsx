2a0ad4a7e711f7a7e96b477f3c8458abe3849587
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-10 15:07:11 +0530

    fetched data from database (firebase) successfully

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import { v4 as uuidv4 } from "uuid";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 const Ingredients = () => {
   const [userIngredient, setUserIngredient] = useState([]);
+
+  // Fetching Data from database
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-49032-default-rtdb.firebaseio.com/IngredientsFromInput.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredient = [];
+        for (const key in responseData) {
+          loadedIngredient.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredient(loadedIngredient);
+      });
+  }, []);
   // Adding Ingredient Into List
   // const ingredientGUID = uuidv4();
   const addIngredientsHandler = (Ingredients) => {
