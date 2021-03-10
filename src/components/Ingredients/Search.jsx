4e339c6daccbfe9572cf758e1d6af452ac695ed0
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-10 21:50:58 +0530

    fetching data from fire base in search component

--- src/components/Ingredients/Search.jsx
+++ src/components/Ingredients/Search.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
 
 const Search = React.memo((props) => {
   const [enteredFilterData, setEnteredFilterData] = useState("");
-
+  useEffect(() => {}, [enteredFilterData]);
   return (
     <section className="search">
       <Card>
