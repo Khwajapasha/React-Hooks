4183d85d6ba8c1e7896a193e1cba2548f3fc8eff
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-10 21:50:09 +0530

    working in search component for filtering data

--- src/components/Ingredients/Search.jsx
+++ src/components/Ingredients/Search.jsx
@@ -5,12 +5,17 @@ import "./Search.css";
 
 const Search = React.memo((props) => {
   const [enteredFilterData, setEnteredFilterData] = useState("");
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredFilterData || ""}
+            onChange={(e) => setEnteredFilterData(e.target.value)}
+          />
         </div>
       </Card>
     </section>
