b7ae7acbf0bf0d0e07ff22b1c7c9dcc5c37ca478
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-10 21:43:13 +0530

    removed extra code

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -26,22 +26,8 @@ const Ingredients = () => {
   }, []);
   useEffect(() => {
     console.log("testing useEffect==>");
-  });
-  fetch(
-    "https://react-hooks-49032-default-rtdb.firebaseio.com/IngredientsFromInput.json"
-  )
-    .then((response) => response.json())
-    .then((responseData) => {
-      const loadedIngredient = [];
-      for (const key in responseData) {
-        loadedIngredient.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount,
-        });
-      }
-      setUserIngredient(loadedIngredient);
-    });
+  }, []);
+
   // Adding Ingredient Into List
   // const ingredientGUID = uuidv4();
   const addIngredientsHandler = (Ingredients) => {
