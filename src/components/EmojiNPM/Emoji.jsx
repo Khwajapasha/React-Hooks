d08a0b2a9adb6e61232098c23864a240960ab71b
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-11 23:24:40 +0530

    comment out all code

--- src/components/EmojiNPM/Emoji.jsx
+++ src/components/EmojiNPM/Emoji.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-import TextAreaEmoji from "react-input-emoji";
+// import TextAreaEmoji from "react-input-emoji";
 
-const Emoji = () => {
-  const [text, setText] = useState("");
+// const Emoji = () => {
+//   const [text, setText] = useState("");
 
-  function handleOnEnter(text) {
-    console.log("enter", text);
-  }
-  return (
-    <div>
-      <TextAreaEmoji
-        value={text}
-        onChange={setText}
-        cleanOnEnter
-        onEnter={handleOnEnter}
-        placeholder="Type a message"
-      />
-    </div>
-  );
-};
+//   function handleOnEnter(text) {
+//     console.log("enter", text);
+//   }
+//   return (
+//     <div>
+//       <TextAreaEmoji
+//         value={text}
+//         onChange={setText}
+//         cleanOnEnter
+//         onEnter={handleOnEnter}
+//         placeholder="Type a message"
+//       />
+//     </div>
+//   );
+// };
 
-export default Emoji;
+// export default Emoji;
