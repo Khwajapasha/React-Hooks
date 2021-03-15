26e315412e77a37f6d4df28cc575bbf882ee579a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-16 01:39:27 +0530

    removed double fetching from DB from ingredient already fetching in search comp

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -40,24 +40,6 @@ const Ingredients = () => {
   }); // Using useReducer
   // const [isLoading, setIsLoading] = useState(false);
   // const [showError, setShowError] = useState();
-  // Fetching Data from database (FireBase)
-  // useEffect(() => {
-  //   fetch(
-  //     "https://react-hooks-49032-default-rtdb.firebaseio.com/IngredientsFromInput.json"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((responseData) => {
-  //       const loadedIngredient = [];
-  //       for (const key in responseData) {
-  //         loadedIngredient.push({
-  //           id: key,
-  //           title: responseData[key].title,
-  //           amount: responseData[key].amount,
-  //         });
-  //       }
-  //       setUserIngredient(loadedIngredient);
-  //     });
-  // }, []);
 
   // useEffect for testing purpose
   useEffect(() => {
