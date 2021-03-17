8e7e7be53c08c6c3b8b7e4ab25baa58c27ef900a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-17 21:46:17 +0530

    woking with useContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Ingredients from "./components/Ingredients/Ingredients";
-
+import Auth from "./components/Auth";
 const App = (props) => {
   return (
     <div>
