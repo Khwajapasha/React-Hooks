36677c8f83e9bfccba7a442a91abfe177ac5fd59
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-17 22:15:23 +0530

    working with auth context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 
-// import Ingredients from "./components/Ingredients/Ingredients";
+import Ingredients from "./components/Ingredients/Ingredients";
 import Auth from "./components/Auth";
 import { AuthContext } from "./Context/AuthContext";
 const App = (props) => {
+  const authContext = useContext(AuthContext);
+  let content = <Auth />;
+  if (authContext.isAuth) {
+    content = <Ingredients />;
+  }
   return (
     <div>
       {/* <Ingredients /> */}
