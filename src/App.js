8d23eb083b7a5e73b5083dfad84e706d9651c56a
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-17 22:12:47 +0530

    working on useContext

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // import Ingredients from "./components/Ingredients/Ingredients";
 import Auth from "./components/Auth";
-// import { AuthContext } from "./Context/AuthContext";
+import { AuthContext } from "./Context/AuthContext";
 const App = (props) => {
   return (
     <div>
