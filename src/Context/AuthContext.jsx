461e0b7deb7bfd092ad1d12dcdbeed833f447697
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-03-17 21:53:51 +0530

    working with userAuthentication

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSlate } from "slate-react";
 export const AuthContext = React.createContext({
   isAuth: false,
   login: () => {},
 });
 
 const AuthContextProvider = (props) => {
-  return <AuthContext.Provider></AuthContext.Provider>;
+  const [isAuthenticated, setIsAuthenticated] = useSlate(false);
+  return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
 };
